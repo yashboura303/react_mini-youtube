abd1def04aa0a2885b7c55262ed32a126f068db4
yash <yashboura303@gmail.com>
2020-05-13 00:52:55 +0530

    Updates

---
+++ precache-manifest.db60c977785c95b2d3564c449247fa87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20fef16b3cada06fafa5c01c26c56ebc",
+    "url": "/react_mini-youtube/index.html"
+  },
+  {
+    "revision": "4f39d8c85f2071fbbd08",
+    "url": "/react_mini-youtube/static/css/main.0a795dae.chunk.css"
+  },
+  {
+    "revision": "a183d8c064ba8c872c0a",
+    "url": "/react_mini-youtube/static/js/2.9a5a45ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_mini-youtube/static/js/2.9a5a45ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f39d8c85f2071fbbd08",
+    "url": "/react_mini-youtube/static/js/main.376633c7.chunk.js"
+  },
+  {
+    "revision": "1d316d551cccd6367c7a",
+    "url": "/react_mini-youtube/static/js/runtime-main.e1cb6eee.js"
+  }
+]);
